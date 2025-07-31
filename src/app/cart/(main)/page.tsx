586cb35c06aaa9a23f353fac6cd76dae85825b5f
batorgil123 <imalreadylost9@gmail.com>
2025-07-31 13:17:08 +0800

    fixed design

--- src/app/cart/(main)/page.tsx
+++ src/app/cart/(main)/page.tsx
@@ -5,24 +5,22 @@ import { Suspense } from "react";
 
 export default function CartPage() {
   return (
-    <Suspense
-      fallback={
-        <div className="w-full h-screen flex items-center justify-center">
-          Loading...
-        </div>
-      }
-    >
-      <div className="w-full h-screen bg-gray-200 flex flex-col">
-        <div className="w-full px-[28px] flex flex-col items-center justify-center p-8">
+    <div className="w-full h-screen bg-gray-200 flex flex-col">
+      <div className="w-full px-[28px] flex flex-col items-center justify-center p-8">
+        <Suspense fallback={<div>Loading header...</div>}>
           <CartHeader />
-          <div className="w-full flex flex-row justify-between">
-            <div className="w-3/4 flex flex-row items-center gap-8 py-8">
+        </Suspense>
+        <div className="w-full flex flex-row justify-between">
+          <div className="w-3/4 flex flex-row items-center gap-8 py-8">
+            <Suspense fallback={<div>Loading header...</div>}>
               <CartItems />
-            </div>
-            <CartClientWrapper />
+            </Suspense>
           </div>
+          <Suspense fallback={<div>Loading header...</div>}>
+            <CartClientWrapper />
+          </Suspense>
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 }
